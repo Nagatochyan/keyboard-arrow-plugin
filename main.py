63852f9ed8e2e0864cc05b0d1ac3558a1567017a
MaxaM <94958239+Nagatochyan@users.noreply.github.com>
2023-02-07 20:23:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import keyboard
 import pyautogui
 import pyperclip
 def test():
-  if keyboard.is_pressed("esc+right"):
+  if keyboard.is_pressed("shift+right"):
     pyperclip.copy("→")
     pyautogui.hotkey('ctrl', 'v')
-  if keyboard.is_pressed("esc+left"):
+  if keyboard.is_pressed("shift+left"):
     pyperclip.copy("←")
     pyautogui.hotkey('ctrl', 'v')
-  if keyboard.is_pressed("esc+up"):
+  if keyboard.is_pressed("shift+up"):
     pyperclip.copy("↑")
     pyautogui.hotkey('ctrl', 'v')
-  if keyboard.is_pressed("esc+down"):
+  if keyboard.is_pressed("shift+down"):
     pyperclip.copy("↓")
     pyautogui.hotkey('ctrl', 'v')
 while True:
   test()
-    
